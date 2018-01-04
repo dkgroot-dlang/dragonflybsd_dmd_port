3fee354e5d63fe341cfbd972547b39e2c3d014fb
Diederik de Groot <ddegroot@talon.nl>
2018-01-04 13:20:39 +0100

    Update compile script

--- compile.sh
+++ compile.sh
@@ -4,110 +4,111 @@ export NCPU=${NCPU:-2}
 export OS=${OS:-dragonflybsd}
 export MODEL=${MODEL:-64}
 export MAKE=gmake
+export GITUSER=dkgroot
 
 bootstrap() {
-    cd ${DMD_BASEDIR}
-    CURSTAGE=bootstrap
+	cd ${DMD_BASEDIR}
+	CURSTAGE=bootstrap
 
-    echo "Running ${CURSTAGE} Compilation (NCPU:$NCPU / OS:$OS / MODEL:$MODEL)..."
-    [ ! -d ${CURSTAGE} ] && mkdir ${CURSTAGE}
-    pushd ${CURSTAGE}
-            #if [ ! -d dmd ]; then
-            #         git clone -b v2.067.1 https://github.com/dlang/dmd.git
-            #         cd dmd
-            #         git apply --reject ${DMD_BASEDIR}/patches/v2.067.1.patch
-            #         mkdir -p ini/dragonflybsd/bin64
-            #         cp ini/freebsd/bin64/dmd.conf ini/dragonflybsd/bin64/
-            #         cd ..
-            #fi
-            [ ! -d dmd ] && git clone -b dragonflybsd_v2.067.1 https://github.com/dlang/dmd.git
-            [ ! -d druntime ] && git clone -b dmd-cxx https://github.com/dlang/druntime.git
-            [ ! -d phobos ] && git clone -b dmd-cxx https://github.com/dlang/phobos.git
-            pushd dmd
-                    $MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ -j${NCPU} $*
-            popd
-            for dir in druntime phobos; do
-                    pushd ${dir}
-                            echo "-----------------------------------------------------------------------------------------------------"
-                            echo "running: $MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ $*"
-                            echo "-----------------------------------------------------------------------------------------------------"
-                            $MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ $*
-                    popd
-            done
-    popd
+	echo "Running ${CURSTAGE} Compilation (NCPU:$NCPU / OS:$OS / MODEL:$MODEL)..."
+	[ ! -d ${CURSTAGE} ] && mkdir ${CURSTAGE}
+	pushd ${CURSTAGE}
+		#if [ ! -d dmd ]; then
+		#	 git clone -b v2.067.1 https://github.com/dlang/dmd.git
+		#	 cd dmd
+		#	 git apply --reject ${DMD_BASEDIR}/patches/v2.067.1.patch
+		#	 mkdir -p ini/dragonflybsd/bin64
+		#	 cp ini/freebsd/bin64/dmd.conf ini/dragonflybsd/bin64/
+		#	 cd ..
+		#fi
+		[ ! -d dmd ] && git clone -b dragonflybsd_v2.067.1 https://github.com/${GITUSER}/dmd.git
+		[ ! -d druntime ] && git clone -b dmd-cxx https://github.com/${GITUSER}/druntime.git
+		[ ! -d phobos ] && git clone -b dmd-cxx https://github.com/${GITUSER}/phobos.git
+		pushd dmd
+			$MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ -j${NCPU} $*
+		popd
+		for dir in druntime phobos; do
+			pushd ${dir}
+				echo "-----------------------------------------------------------------------------------------------------"
+				echo "running: $MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ $*"
+				echo "-----------------------------------------------------------------------------------------------------"
+				$MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ $*
+			popd
+		done
+	popd
 }
 
 master() {
-    cd ${DMD_BASEDIR}
-    CURSTAGE=master
+	cd ${DMD_BASEDIR}
+	CURSTAGE=master
 
-    echo "Running ${CURSTAGE} Compilation (NCPU:$NCPU / OS:$OS / MODEL:$MODEL)..."
-    [ ! -d ${CURSTAGE} ] && mkdir ${CURSTAGE}
-    pushd ${CURSTAGE}
-            [ ! -d dmd ] && git clone -b dragonflybsd-master https://github.com/dlang/dmd.git
-            [ ! -d druntime ] && git clone -b dragonflybsd-master https://github.com/dlang/druntime.git
-            [ ! -d phobos ] && git clone -b dragonflybsd-master https://github.com/dlang/phobos.git
-            export HOST_DMD=${DMD_BASEDIR}/bootstrap/install/${OS}/bin${MODEL}/dmd
-            pushd dmd
-                    echo "-----------------------------------------------------------------------------------------------------"
-                    echo "running: $MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ -j${NCPU} HOST_DMD=${HOST_DMD} $*"
-                    echo "-----------------------------------------------------------------------------------------------------"
-                    $MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ -j${NCPU} HOST_DMD=${HOST_DMD} $*
-            popd
-            for dir in druntime phobos; do
-                    pushd ${dir}
-                            echo "-----------------------------------------------------------------------------------------------------"
-                            echo "running: $MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ HOST_DMD=${HOST_DMD} $*"
-                            echo "-----------------------------------------------------------------------------------------------------"
-                            $MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ HOST_DMD=${HOST_DMD} $*
-                    popd
-            done
-    popd
+	echo "Running ${CURSTAGE} Compilation (NCPU:$NCPU / OS:$OS / MODEL:$MODEL)..."
+	[ ! -d ${CURSTAGE} ] && mkdir ${CURSTAGE}
+	pushd ${CURSTAGE}
+		[ ! -d dmd ] && git clone -b dragonflybsd-master https://github.com/${GITUSER}/dmd.git
+		[ ! -d druntime ] && git clone -b dragonflybsd-master https://github.com/${GITUSER}/druntime.git
+		[ ! -d phobos ] && git clone -b dragonflybsd-master https://github.com/${GITUSER}/phobos.git
+		export HOST_DMD=${DMD_BASEDIR}/bootstrap/install/${OS}/bin${MODEL}/dmd
+		pushd dmd
+			echo "-----------------------------------------------------------------------------------------------------"
+			echo "running: $MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ -j${NCPU} HOST_DMD=${HOST_DMD} $*"
+			echo "-----------------------------------------------------------------------------------------------------"
+			$MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ -j${NCPU} HOST_DMD=${HOST_DMD} $*
+		popd
+		for dir in druntime phobos; do
+			pushd ${dir}
+				echo "-----------------------------------------------------------------------------------------------------"
+				echo "running: $MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ HOST_DMD=${HOST_DMD} $*"
+				echo "-----------------------------------------------------------------------------------------------------"
+				$MAKE -f posix.mak MODEL=${MODEL} HOST_CSS=g++ HOST_DMD=${HOST_DMD} $*
+			popd
+		done
+	popd
 }
 
 tools() {
 	CURSTAGE=master
 	pushd ${CURSTAGE}
-        if [ ! -d tools ]; then
-        	git clone https://github.com/dlang/tools.git
-        	cd tools
-        	git apply --reject ../../patches/tools.patch
-        	cd ..
-        fi
-        pushd tools
-        echo "-----------------------------------------------------------------------------------------------------"
-        echo "building tools..."
-        echo "-----------------------------------------------------------------------------------------------------"
-        $MAKE -f posix.mak MODEL=${MODEL} OS=${OS}
-        popd
-        popd
+	if [ ! -d tools ]; then
+		git clone https://github.com/${GITUSER}/tools.git
+		cd tools
+		git apply --reject ../../patches/tools.patch
+		cd ..
+	fi
+	pushd tools
+	echo "-----------------------------------------------------------------------------------------------------"
+	echo "building tools..."
+	echo "-----------------------------------------------------------------------------------------------------"
+	$MAKE -f posix.mak MODEL=${MODEL} OS=${OS}
+	popd
+	popd
 }
 
 dub_unittest() {
 	CURSTAGE=master
 	pushd ${CURSTAGE}
-        if [ ! -d dub ]; then
-		git clone https://github.com/dlang/dub.git
-		cd dub
-        	git apply --reject ../../patches/dub.patch
-        	cd ..
-        fi
-        pushd dub
-        echo "-----------------------------------------------------------------------------------------------------"
-        echo "building dub..."
-        echo "-----------------------------------------------------------------------------------------------------"
-        DMD=${DMD_BASEDIR}/master/install/dragonflybsd/bin64/dmd ./build.sh
-        echo "-----------------------------------------------------------------------------------------------------"
-        echo "running unittests..."
-        echo "-----------------------------------------------------------------------------------------------------"
-        DUB=${DMD_BASEDIR}/master/dub/bin/dub DC=${DMD_BASEDIR}/master/install/dragonflybsd/bin64/dmd test/run-unittest.sh
-        popd
-        popd
+	if [ ! -d dub ]; then
+	git clone https://github.com/${GITUSER}/dub.git
+	cd dub
+		git apply --reject ../../patches/dub.patch
+		cd ..
+	fi
+	pushd dub
+	echo "-----------------------------------------------------------------------------------------------------"
+	echo "building dub..."
+	echo "-----------------------------------------------------------------------------------------------------"
+	DMD=${DMD_BASEDIR}/master/install/dragonflybsd/bin64/dmd ./build.sh
+	echo "-----------------------------------------------------------------------------------------------------"
+	echo "running unittests..."
+	echo "-----------------------------------------------------------------------------------------------------"
+	DUB=${DMD_BASEDIR}/master/dub/bin/dub DC=${DMD_BASEDIR}/master/install/dragonflybsd/bin64/dmd test/run-unittest.sh
+	popd
+	popd
 }
 
 # Generate bootstrap dmd compile
-bootstrap clean
-bootstrap DEBUG=1 BUILD=debug ENABLE_DEBUG=1 install
+#bootstrap clean
+#bootstrap DEBUG=1 BUILD=debug ENABLE_DEBUG=1 install
 
 # Generate debug dmd compiler
 master clean
